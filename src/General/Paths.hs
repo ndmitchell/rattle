9d84a02ed3efa9b31cfa6e52f1f30253eb903955
Neil Mitchell <ndmitchell@gmail.com>
2019-04-25 16:51:14 +0100

    Delete unneeded things from Paths

--- src/General/Paths.hs
+++ src/General/Paths.hs
@@ -2,18 +2,14 @@
 -- | The information from Paths_rattle cleaned up
 module General.Paths(
     rattleVersionString,
-    initDataDirectory,
-    hasManualData, copyManualData,
     readDataFileHTML
     ) where
 
 import Paths_rattle
-import Control.Exception
 import Control.Monad.Extra
 import Data.Version
 import System.Directory
 import System.FilePath
-import System.Info.Extra
 import System.IO.Unsafe
 import System.Environment
 import General.Extra
@@ -35,11 +31,6 @@ dataDirs = unsafePerformIO $ do
     return $ [datdir] ++ [exedir | exedir /= ""] ++ [curdir]
 
 
--- The data files may be located relative to the current directory, if so cache it in advance
-initDataDirectory :: IO ()
-initDataDirectory = void $ evaluate dataDirs
-
-
 getDataFile :: FilePath -> IO FilePath
 getDataFile file = do
     let poss = map (</> file) dataDirs
@@ -48,23 +39,6 @@ getDataFile file = do
         [] -> fail $ unlines $ ("Could not find data file " ++ file ++ ", looked in:") : map ("  " ++) poss
         x:_ -> return x
 
-hasDataFile :: FilePath -> IO Bool
-hasDataFile file = anyM (\dir -> doesFileExist_ $ dir </> file) dataDirs
-
 
 readDataFileHTML :: FilePath -> IO LBS.ByteString
 readDataFileHTML file = LBS.readFile =<< getDataFile ("html" </> file)
-
-
-manualFiles :: [FilePath]
-manualFiles = map ("docs/manual" </>) ["Build.hs","main.c","constants.c","constants.h","build" <.> if isWindows then "bat" else "sh"]
-
-hasManualData :: IO Bool
-hasManualData = allM hasDataFile manualFiles
-
-copyManualData :: FilePath -> IO ()
-copyManualData dest = do
-    createDirectoryRecursive dest
-    forM_ manualFiles $ \file -> do
-        src <- getDataFile file
-        copyFile src (dest </> takeFileName file)
