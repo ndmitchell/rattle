4f0d37863b70b11d4afe36f94aef5c370c7e7b72
Neil Mitchell <ndmitchell@gmail.com>
2019-05-21 23:12:28 +0100

    More work on the stack demo

--- test/Test/Example/Stack.hs
+++ test/Test/Example/Stack.hs
@@ -6,40 +6,52 @@ import Development.Rattle
 import Development.Shake.FilePath
 import System.IO.Extra
 import qualified Data.ByteString as BS
+import Data.Maybe
+import Control.Monad.Extra
+import System.Environment
+import Data.List.Extra
+import qualified Data.HashMap.Strict as Map
+
+-- Cabal packages
 import Distribution.PackageDescription.Parsec
 import Distribution.Types.CondTree
 import Distribution.Types.GenericPackageDescription
 import Distribution.Types.Dependency
 import Distribution.Types.PackageName(unPackageName)
-import Data.Maybe
-import Control.Monad.Extra
-import Data.List.Extra
-import qualified Data.HashMap.Strict as Map
 
 
 type PackageName = String
 type PackageVersion = String
 
 main :: IO ()
-main = rattle rattleOptions $ stack "nightly-2019-05-15" ["aeson","shake"]
+main = do
+    args <- getArgs
+    let ignore = ["**/hackage-security-lock", "**/package.cache.lock"]
+    rattle rattleOptions{rattleIgnore=ignore} $ stack "nightly-2019-05-15" $ args ++ ["cereal" | null args]
 
 
-installPackage :: (PackageName -> Run ()) -> FilePath -> PackageName -> PackageVersion -> Run ()
+installPackage :: (PackageName -> Run (Maybe PackageVersion)) -> FilePath -> PackageName -> PackageVersion -> Run ()
 installPackage dep config name version = do
-    cmd "cabal unpack" (name ++ "-" ++ version)
-    depends <- liftIO $ cabalDepends $ name ++ "-" ++ version </> name <.> "cabal"
-    forP_ depends dep
-    cmd "cabal configure"
-    cmd "cabal build"
+    let dir = name ++ "-" ++ version
+    cmd "cabal unpack" dir
+    depends <- liftIO $ cabalDepends $ dir </> name <.> "cabal"
+    dependsVer <- forP depends dep
+    cmd (Cwd dir) "cabal v1-configure"
+        "--disable-library-profiling"
+        ["--package-db=../" ++ n ++ "-" ++ v ++ "/dist/package.conf.inplace" | (n, Just v) <- zip depends dependsVer]
+    cmd (Cwd dir) "cabal v1-build" ("lib:" ++ name)
 
 
 stack :: String -> [PackageName] -> Run ()
-stack resolver packages = when False $ withIgnore ["**/hackage-security-lock"] $ do
+stack resolver packages = do
     let config = resolver <.> "config"
     cmd "curl" ("https://www.stackage.org/" ++ resolver ++ "/cabal.config") "-o" config
     versions <- liftIO $ readResolver config
     let askVersion x = fromMaybe (error $ "Don't know version for " ++ show x) $ Map.lookup x versions
-    needPkg <- memoRec $ \needPkg name -> whenJust (askVersion name) $ installPackage needPkg config name
+    needPkg <- memoRec $ \needPkg name -> do
+        let v = askVersion name
+        whenJust v $ installPackage needPkg config name
+        return v
     forP_ packages needPkg
 
 
