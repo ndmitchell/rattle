307e6f16984f01a4cf2a5c23c1aaaec68ea21b34
Neil Mitchell <ndmitchell@gmail.com>
2019-05-26 13:13:57 +0100

    Add a pipeline runner to get better compatibility

---
+++ pipeline/Pipeline.hs
@@ -0,0 +1,27 @@
+
+-- Run commands separated by
+-- a ; b - do a, continue with b ignoring the escape code
+-- a || b - do a, only do b if a fails
+-- a && b - do a, only do b if a succeeds
+-- there is no bracketing
+module Pipeline(main) where
+
+import Development.Shake.Command
+import System.Environment
+import System.Exit
+
+
+main :: IO ()
+main = exitWith =<< run =<< getArgs
+
+
+isSpecial x = x `elem` [";", "&&", "||"]
+
+
+run :: [String] -> IO ExitCode
+run xs | (a, op:b) <- break isSpecial xs = case op of
+    ";" -> run a >> run b
+    "&&" -> do a <- run a; if a == ExitSuccess then run b else return a
+    "||" -> do a <- run a; if a == ExitSuccess then return a else run b
+run [] = return ExitSuccess
+run (x:xs) = cmd x xs
