f01b807289148d34f54e2af92c2bfce6f0ce7e6b
Neil Mitchell <ndmitchell@gmail.com>
2019-09-29 21:24:31 +0100

    Remove rattleIgnore, can be done with rattleCmdOptions

--- test/Test/Example/Stack.hs
+++ test/Test/Example/Stack.hs
@@ -63,7 +63,7 @@ main = do
     unsetEnv "GHC_PACKAGE_PATH"
     tdir <- canonicalizePath =<< getTemporaryDirectory
     let ignore = ["**/hackage-security-lock", "**/package.cache.lock", tdir ++ "/**"]
-    rattleRun rattleOptions{rattleIgnore=ignore} $
+    rattleRun rattleOptions{rattleCmdOptions=[toCmdOption $ Ignored ignore]} $
         stack "nightly-2019-09-29" $ args ++ ["cereal" | null args]
 
 {-
