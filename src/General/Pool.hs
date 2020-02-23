18ab50c7521017e3ffda675e50e7b81fb1393a39
Neil Mitchell <ndmitchell@gmail.com>
2020-02-23 15:54:45 +0000

    Add docs to Pool

--- src/General/Pool.hs
+++ src/General/Pool.hs
@@ -8,7 +8,7 @@ module General.Pool(
     runPoolMaybe
     ) where
 
-import Control.Exception
+import Control.Exception.Extra
 import General.Limit
 import Control.Monad.Extra
 import Control.Concurrent.Extra
@@ -37,13 +37,13 @@ a `finally2` sequel =
     return r
 
 
-spawn :: Var (Maybe (Set.HashSet (Async ()))) -> IO a -> IO a
+spawn :: Var (Maybe (Set.HashSet (Async ()))) -> IO a -> IO (Maybe a)
 spawn var act = mask $ \restore1 -> uninterruptibleMask $ \restore2 -> do
     -- Important we do uninterruptibleMask since spawning async is interruptible
     -- and after we have spawn'd async its important it ends up in the mp so it can be cancelled
     -- However, relax that during cleanup, since if someone wants to abort cleanup its their choice
     (thing, after) <- modifyVar var $ \case
-        Nothing -> return (Nothing, (fail "runPool after pool has terminated", return ()))
+        Nothing -> return (Nothing, (return Nothing, return ()))
         Just mp -> do
             a <- async $ restore1 act
             let undo = modifyVar_ var $ \case
@@ -52,14 +52,16 @@ spawn var act = mask $ \restore1 -> uninterruptibleMask $ \restore2 -> do
                         mp <- evaluate $ Set.delete (void a) mp
                         return $ Just mp
             mp <- evaluate $ Set.insert (void a) mp
-            return (Just mp, (wait a, cancel a >> undo))
+            return (Just mp, (Just <$> wait a, cancel a >> undo))
     r <- restore1 thing `onException` restore2 after
     restore2 after
     return r
 
-
+-- | Run on the Pool, block as long as required. Fails if the pool is shutting down.
 runPool :: Pool -> IO a -> IO a
-runPool (Pool limit threads) = withLimit limit . spawn threads
+runPool (Pool limit threads) = fromMaybeM err . withLimit limit . spawn threads
+    where err = errorIO "runPool after pool has terminated"
 
+-- | Run on the Pool if the pool hasn't shut down and has an available slot.
 runPoolMaybe :: Pool -> IO a -> IO (Maybe a)
-runPoolMaybe (Pool limit threads) = withLimitMaybe limit . spawn threads
+runPoolMaybe (Pool limit threads) = fmap join . withLimitMaybe limit . spawn threads
