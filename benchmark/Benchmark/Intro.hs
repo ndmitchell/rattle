972c97fad6fea2f3ea5a9b51cf1636480dca0e26
Neil Mitchell <ndmitchell@gmail.com>
2020-02-23 22:41:37 +0000

    Make the intro test check non-significant changes too

--- benchmark/Benchmark/Intro.hs
+++ benchmark/Benchmark/Intro.hs
@@ -11,6 +11,18 @@ import System.Directory
 import Development.Shake.Command
 import Development.Rattle
 
+
+
+writeMain :: Int -> Int -> IO ()
+writeMain real fake = writeFile "main.c" $ unlines
+    ["#include <stdio.h>"
+    ,"char* util();"
+    ,"void main(){printf(\"%s" ++ replicate real ' ' ++ "\", util());}" ++ replicate fake ' ']
+
+writeUtil :: Int -> Int -> IO ()
+writeUtil real fake = writeFile "util.c" $ unlines
+    ["char* util(){return \"test" ++ replicate real ' ' ++ "\";}" ++ replicate fake ' ']
+
 main :: Args -> IO ()
 main Args{..} = withTempDir $ \dir -> withCurrentDirectory dir $ do
     writeFile "gcc.sh" "sleep 1 && gcc $*"
@@ -28,12 +40,6 @@ main Args{..} = withTempDir $ \dir -> withCurrentDirectory dir $ do
         ,"./gcc.sh -c util.c"
         ,"./gcc.sh -o main.exe main.o util.o"
         ]
-    writeFile "main.c" $ unlines
-        ["#include <stdio.h>"
-        ,"char* util();"
-        ,"void main(){printf(\"%s\", util());}"]
-    writeFile "util.c" $ unlines
-        ["char* util(){return \"test\";}"]
 
     let clean = do
             whenM (doesDirectoryExist ".rattle") $
@@ -43,7 +49,7 @@ main Args{..} = withTempDir $ \dir -> withCurrentDirectory dir $ do
                     removeFile x
 
     forM_ (threads `orNull` [1..4]) $ \j -> do
-        let make = cmd_ "make" ["-j" ++ show j]
+        let make = cmd_ "make" ["-j" ++ show j] (EchoStdout False)
         let opts = rattleOptions{rattleProcesses=j, rattleUI=Just RattleQuiet, rattleNamedDirs=[]}
         let rattle = rattleRun opts $ mapM_ (cmd Shell) rattleCmds
 
@@ -51,11 +57,15 @@ main Args{..} = withTempDir $ \dir -> withCurrentDirectory dir $ do
             putStr $ name ++ " -j" ++ show j ++ ": "
             hFlush stdout
             clean
+            writeMain 0 0
+            writeUtil 0 0
             (t1, _) <- duration act
             (t2, _) <- duration act
-            appendFile "main.c" " "
+            writeMain 0 1
             (t3, _) <- duration act
-            appendFile "main.c" " "
-            appendFile "util.c" " "
+            writeMain 1 1
             (t4, _) <- duration act
-            putStrLn $ unwords $ map showDuration [t1,t2,t3,t4]
+            writeMain 2 1
+            writeUtil 1 0
+            (t5, _) <- duration act
+            putStrLn $ unwords $ map showDuration [t1,t2,t3,t4,t5]
