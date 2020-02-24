285ca8417c18646ccd9f8c72cac6b5ddb4372502
Neil Mitchell <ndmitchell@gmail.com>
2020-02-24 14:14:21 +0000

    Fix a variable number error

--- src/Development/Rattle/Server.hs
+++ src/Development/Rattle/Server.hs
@@ -165,11 +165,11 @@ modifyS rattle@Rattle{..} act = join $ modifyVar state $ \case
             Right (Just s) -> return (Right $ ensureS s, runSpeculate rattle >> cont)
 
 ensureS :: S -> S
-ensureS s0 = s2
+ensureS = fillInNext . reduceSpeculate
     where
         -- often we drain the front of the speculatable list repeatedly, so do that once
-        s1 = s0{speculatable = dropWhile (\(c, _) -> c `Map.member` started s0) $ speculatable s1}
-        s2 = s1{speculateNext = calculateSpeculateNext s1}
+        reduceSpeculate s = s{speculatable = dropWhile (\(c, _) -> c `Map.member` started s) $ speculatable s}
+        fillInNext s = s{speculateNext = calculateSpeculateNext s}
 
 
 -- speculate on a process iff it is the first process in speculate that:
